1ed358ed9cce1cac216b62d214fc906a035bdbb6
NobodyForNothing <contact@derdilla.com>
2024-12-03 09:44:23 +0100

    commit to code sharing and stable architecture

---
+++ aoc_solver.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+abstract class AOCSolver<T> {
+  /// Day as a 2 digit String like '10', or '03'.
+  String get day;
+
+  void main() {
+    print('[--------------- Running tests ---------------]');
+    if (test()) return;
+    //print('\\---------------------------------------------/');
+
+    print('[--------------- Solve puzzles ---------------]');
+    print('  A: ${solveA(input)}');
+    print('  B: ${solveB(input)}');
+    //print('\\---------------------------------------------/');
+  }
+
+  /// Run KATs on tests and returns true on failure.
+  bool test() {
+    bool failed = false;
+
+    String res = solveA(katA.$1);
+    if (res != katA.$2) {
+      print('KAT A failed: expected ${katA.$2} but got $res');
+      failed = true;
+    } else print('  ✔️ KAT A passed');
+  
+    res = solveB(katB.$1);
+    if (res != katB.$2) {
+      print('KAT B failed: expected ${katB.$2} but got $res');
+      failed = true;
+    } else print('  ✔️ KAT B passed');
+
+    return failed;
+  }
+
+  String solveA(String input) {
+    final parsed = parseA(input);
+    return computeA(parsed);
+  }
+  String solveB(String input) {
+    final parsed = parseB(input);
+    return computeB(parsed);
+  }
+
+  /// First parsing step of the data.
+  T parse(String input);
+  T parseA(String input) => parse(input);
+  T parseB(String input) => parse(input);
+
+  /// Create result of task a from parsed data.
+  String computeA(T input);
+  /// Create result of task b from parsed data.
+  String computeB(T input);
+
+  (String, String) get katA => _readInGenericKAT('a');
+  (String, String) get katB => _readInGenericKAT('b');
+
+  (String, String) _readInGenericKAT(String variant) {
+    final katIn = File('kat/$day.$variant.in').readAsStringSync();
+    final katOut = File('kat/$day.$variant.out').readAsStringSync();
+    return (katIn, katOut);
+  }
+
+  String get input => File('in/$day.in').readAsStringSync();
+
+  bool doLogging = true;
+
+  void log(Object? data) {
+    if(doLogging) print(data);
+  }
+}
--- src/03.dart
+++ src/03.dart
@@ -1,76 +1,66 @@
-import 'dart:io';
+import '../aoc_solver.dart';
 
-int task1(String input) {
-  final regex = RegExp(r"mul\(([0-9]*),([0-9]*)\)");
-  final matches = regex.allMatches(input);
-  print(matches.map((e) => e.groups([0,1,2])));
-  
-  int sumOfProducts = 0;
-  for (final m in matches) {
-    final left = int.parse(m.group(1)!);
-    final right = int.parse(m.group(2)!);
-    print('+ $left * $right');
-    sumOfProducts += (left * right);
-  }
-
-  return sumOfProducts;
+void main(List<String> _args) {
+  final solver = A03()
+    ..doLogging = false;
+  solver.main();
 }
 
-int task2(String input) {
-  final regex = RegExp(r"(?:do\(\))|(?:don't\(\))|mul\(([0-9]*),([0-9]*)\)");
-  final matches = regex.allMatches(input);
-  print(matches.map((e) => e.groups([0])));
-  
-  int sumOfProducts = 0;
-  bool ignore = false;
-  for (final m in matches) {
-    if(m.group(0) == 'do()') {
-      ignore = false;
-      print('Disable Ignore');
-      continue;
-    } else if(m.group(0) == "don't()") {
-      ignore = true;
-      print('Enable Ignore');
-      continue;
-    } 
+class A03 extends AOCSolver<String> {
+  @override
+  String parse(String input) => input;
 
-
-    final left = int.parse(m.group(1)!);
-    final right = int.parse(m.group(2)!);
-    if (!ignore) {
-      print('+ $left * $right');
-      sumOfProducts += (left * right);
-    } else {
-      print('ignore $left * $right ');
-    }
+  @override
+  String computeA(String input) {
+    final regex = RegExp(r"mul\(([0-9]*),([0-9]*)\)");
+    final matches = regex.allMatches(input);
+    log(matches.map((e) => e.groups([0,1,2])));
     
+    int sumOfProducts = 0;
+    for (final m in matches) {
+      final left = int.parse(m.group(1)!);
+      final right = int.parse(m.group(2)!);
+      log('+ $left * $right');
+      sumOfProducts += (left * right);
+    }
+
+    return sumOfProducts.toString();
   }
 
-  return sumOfProducts;
-}
+  @override
+  String computeB(String input) {
+    final regex = RegExp(r"(?:do\(\))|(?:don't\(\))|mul\(([0-9]*),([0-9]*)\)");
+    final matches = regex.allMatches(input);
+    log(matches.map((e) => e.groups([0])));
+    
+    int sumOfProducts = 0;
+    bool ignore = false;
+    for (final m in matches) {
+      if(m.group(0) == 'do()') {
+        ignore = false;
+        log('Disable Ignore');
+        continue;
+      } else if(m.group(0) == "don't()") {
+        ignore = true;
+        log('Enable Ignore');
+        continue;
+      } 
 
-void main(List<String> args) {
-  /*final catIn = File('cat/03.in').readAsStringSync();
-  final catOut = File('cat/03.out').readAsStringSync();
-  final catRes = task1(catIn);
 
-  if (catOut != catRes.toString()) {
-    print('Failed CAT: expected $catOut got $catRes');
-    return;
+      final left = int.parse(m.group(1)!);
+      final right = int.parse(m.group(2)!);
+      if (!ignore) {
+        log('+ $left * $right');
+        sumOfProducts += (left * right);
+      } else {
+        log('ignore $left * $right ');
+      }
+      
+    }
+
+    return sumOfProducts.toString();
   }
 
-  final input = File('in/03.in').readAsStringSync();
-  print('Real Result: ${task1(input)}');*/
-
-  final catIn = File('cat/03.b.in').readAsStringSync();
-  final catOut = File('cat/03.b.out').readAsStringSync();
-  final catRes = task2(catIn);
-
-  if (catOut != catRes.toString()) {
-    print('Failed CAT 2: expected $catOut got $catRes');
-    return;
-  }
-
-  final input = File('in/03.in').readAsStringSync();
-  print('Real Result: ${task2(input)}');
+  @override
+  String get day => '03';
 }
