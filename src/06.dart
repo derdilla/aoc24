68b9ad3d5063368ecc7ac30f74ed8910f29f82c7
NobodyForNothing <contact@derdilla.com>
2024-12-06 23:45:45 +0100

    Clean up 06 code

--- src/06.dart
+++ src/06.dart
@@ -2,8 +2,8 @@ import '../aoc_solver.dart';
 
 void main(List<String> _args) {
   final solver = A06()
-    ..ignoreTests = false
-    ..doLogging = true;
+    ..ignoreTests = true
+    ..doLogging = false;
   solver.main();
 }
 
@@ -80,115 +80,7 @@ class A06 extends AOCSolver<GuardMap> {
   }
 
   @override
-  String computeB(GuardMap input) {
-    List<(int, int, Dir)> path = [];
-    Set<(int,int)> possibleObstructions = Set();
-
-    int i = 0;
-    while (!path.contains((input.guardPos.$1, input.guardPos.$2, input.facing))
-      && input.guardPos.$1 < input.maxX
-      && input.guardPos.$2 < input.maxY
-      && input.guardPos.$1 >= 0
-      && input.guardPos.$2 >= 0
-    ) {
-      log(i);
-      i++;
-      path.add((input.guardPos.$1, input.guardPos.$2, input.facing));
-      switch (input.facing) {
-        case Dir.up:
-          if (getsBackOnTrack(input, (input.guardPos.$1, input.guardPos.$2, Dir.right), path, (input.guardPos.$1, input.guardPos.$2 - 1))) {
-            possibleObstructions.add((input.guardPos.$1, input.guardPos.$2 - 1));
-          }
-
-          if (input.obscacles.contains((input.guardPos.$1, input.guardPos.$2 - 1))) {
-            input.facing = Dir.right;
-          } else {
-            input.guardPos = (input.guardPos.$1, input.guardPos.$2 - 1);
-          }
-        case Dir.left:
-          if (getsBackOnTrack(input, (input.guardPos.$1, input.guardPos.$2, Dir.up), path, (input.guardPos.$1 -1, input.guardPos.$2))) {
-            possibleObstructions.add((input.guardPos.$1 - 1, input.guardPos.$2));
-          }
-
-          if (input.obscacles.contains((input.guardPos.$1 -1, input.guardPos.$2))) {
-            input.facing = Dir.up;
-          } else {
-            input.guardPos = (input.guardPos.$1 -1 , input.guardPos.$2);
-          }
-        case Dir.right:
-          if (getsBackOnTrack(input, (input.guardPos.$1, input.guardPos.$2, Dir.down), path, (input.guardPos.$1 +1 , input.guardPos.$2))) {
-            possibleObstructions.add((input.guardPos.$1 + 1, input.guardPos.$2));
-          }
-
-          if (input.obscacles.contains((input.guardPos.$1 +1 , input.guardPos.$2))) {
-            input.facing = Dir.down;
-          } else {
-            input.guardPos = (input.guardPos.$1 + 1, input.guardPos.$2);
-          }
-        case Dir.down:
-        if (getsBackOnTrack(input, (input.guardPos.$1, input.guardPos.$2, Dir.left), path, (input.guardPos.$1, input.guardPos.$2 + 1))) {
-            possibleObstructions.add((input.guardPos.$1, input.guardPos.$2 + 1));
-          }
-
-          if (input.obscacles.contains((input.guardPos.$1, input.guardPos.$2 + 1))) {
-            input.facing = Dir.left;
-          } else {
-            input.guardPos = (input.guardPos.$1, input.guardPos.$2 + 1);
-          }
-      }
-    }
-
-    log(possibleObstructions);
-    log(path);
-    print('res potentially - 1');
-    return possibleObstructions.length.toString(); // 1844 too high
-  }
-
-  bool getsBackOnTrack(GuardMap input, (int, int, Dir) pos, List<(int, int, Dir)> prevPath, (int, int) newObstacle) {
-    input = GuardMap(pos.$3, (pos.$1, pos.$2), input.obscacles, input.maxX, input.maxY);
-
-    List<(int, int, Dir)> path = [];
-    while (input.guardPos.$1 < input.maxX
-      && input.guardPos.$2 < input.maxY
-      && input.guardPos.$1 >= 0
-      && input.guardPos.$2 >= 0
-    ) {
-      if (prevPath.contains((input.guardPos.$1, input.guardPos.$2, input.facing))) return true;
-      if (path.contains((input.guardPos.$1, input.guardPos.$2, input.facing))) return true;
-      
-      path.add((input.guardPos.$1, input.guardPos.$2, input.facing));
-      switch (input.facing) {
-        case Dir.up:
-          if (input.obscacles.contains((input.guardPos.$1, input.guardPos.$2 - 1))) {
-            input.facing = Dir.right;
-          } else {
-            input.guardPos = (input.guardPos.$1, input.guardPos.$2 - 1);
-          }
-        case Dir.left:
-          if (input.obscacles.contains((input.guardPos.$1 -1, input.guardPos.$2))) {
-            input.facing = Dir.up;
-          } else {
-            input.guardPos = (input.guardPos.$1 -1 , input.guardPos.$2);
-          }
-        case Dir.right:
-          if (input.obscacles.contains((input.guardPos.$1 +1 , input.guardPos.$2))) {
-            input.facing = Dir.down;
-          } else {
-            input.guardPos = (input.guardPos.$1 + 1, input.guardPos.$2);
-          }
-        case Dir.down:
-          if (input.obscacles.contains((input.guardPos.$1, input.guardPos.$2 + 1))) {
-            input.facing = Dir.left;
-          } else {
-            input.guardPos = (input.guardPos.$1, input.guardPos.$2 + 1);
-          }
-      }
-    }
-
-    return false;
-
-  }
-
+  String computeB(GuardMap input) => 'Too slow, use the rust tool';
 }
 
 class GuardMap {
--- src/d06/src/main.rs
+++ src/d06/src/main.rs
@@ -1,14 +1,14 @@
-use std::{collections::HashSet, fs};
+use std::fs;
 
 // solving of 06/b too slow in dart
 
 fn main() {
+    // parse input
     let input = fs::read_to_string("/home/derdilla/Coding/aoc24/in/06.in").unwrap();
-
 
     let mut obstacles: Vec<(usize, usize)> = Vec::new();
     let mut guard_pos: (usize, usize) = (0,0);
-    let mut facing = Dir::up;
+    let mut facing = Dir::Up;
 
     let lines: Vec<Vec<String>> = input.lines().map(|e| e.split("").map(|e| e.to_string()).collect::<Vec<String>>()).collect();
 
@@ -21,129 +21,101 @@ fn main() {
         }
       }
     }
-    let obstacles = obstacles;
+    let obstacles: Vec<(usize, usize)> = obstacles;
+    let initial_guard_pos = guard_pos;
+    let initial_facing = facing.clone();
 
     let max_x = lines.first().unwrap().len();
     let max_y = lines.len();
 
-    let mut path = Vec::new();
-    let mut possible_spots = HashSet::new();
+    println!("Inputs:\n\tguard_pos: {:?}\n\tmax_x: {}\n\tmax_y: {}", &guard_pos, max_x, max_y);
 
-    let mut i = 0;
+    // compute possible placements
+    let mut possible_spots = Vec::new();
     while guard_pos.0 < max_x
       && guard_pos.1 < max_y {
-      //println!("{} {:?} {:?}", i, &guard_pos, &facing);
-      i += 1;
-
-      path.push((guard_pos.0, guard_pos.1, facing.clone()));
-      match facing {
-        Dir::up => {
-          if obstacles.contains(&(guard_pos.0, guard_pos.1 - 1)) {
-            facing = Dir::right;
-          } else if guard_pos.1 == 0 {
-            break;
-          } else {
-            if gets_stuck(&obstacles, guard_pos, facing.clone(), max_x, max_y, (guard_pos.0, guard_pos.1 - 1)) {
-              possible_spots.insert((guard_pos.0, guard_pos.1 - 1));
-            }
-
-            guard_pos = (guard_pos.0, guard_pos.1 - 1)
+      if let Some(next_pos) = facing.step(guard_pos) {
+        if obstacles.contains(&next_pos) {
+          facing = facing.next();
+        } else {
+          if gets_stuck(&obstacles, initial_guard_pos, initial_facing.clone(), max_x, max_y, next_pos) {
+            possible_spots.push(next_pos); 
           }
-        },
-        Dir::left => {
-          if obstacles.contains(&(guard_pos.0 -1 , guard_pos.1)) {
-            facing = Dir::up;
-          } else if guard_pos.0 == 0 {
-            break;
-          } else {
-            if gets_stuck(&obstacles, guard_pos, facing.clone(), max_x, max_y, (guard_pos.0 - 1, guard_pos.1)) {
-              possible_spots.insert((guard_pos.0 - 1, guard_pos.1));
-            }
-            guard_pos = (guard_pos.0 - 1, guard_pos.1)
-          }
-        },
-        Dir::right => {
-          if obstacles.contains(&(guard_pos.0 + 1, guard_pos.1)) {
-            facing = Dir::down;
-          } else if guard_pos.0 == max_x - 1 {
-            break;
-          }else {
-            if gets_stuck(&obstacles, guard_pos, facing.clone(), max_x, max_y, (guard_pos.0 + 1, guard_pos.1)) {
-              possible_spots.insert((guard_pos.0 + 1, guard_pos.1));
-            }
-            guard_pos = (guard_pos.0 + 1, guard_pos.1)
-          }
-        },
-        Dir::down => {
-          if obstacles.contains(&(guard_pos.0, guard_pos.1 + 1)) {
-            facing = Dir::left;
-          } else if guard_pos.0 == max_x - 1 {
-            break;
-          } else {
-            if gets_stuck(&obstacles, guard_pos, facing.clone(), max_x, max_y, (guard_pos.0, guard_pos.1 + 1)) {
-              possible_spots.insert((guard_pos.0, guard_pos.1 + 1));
-            }
-            guard_pos = (guard_pos.0, guard_pos.1 + 1)
-          }
-        },
+          guard_pos = next_pos;
+        }
+      } else {
+        break; // reached map end top/left
       }
     }
 
-    println!("{}", possible_spots.len() - 50); // Not at guard starting pos, counted from file
+    // dedupe possible_spots
+    let mut unique_possible_spots = Vec::new();
+    for s in possible_spots {
+      if s.0 < max_x
+          && s.1 < max_y
+          && !unique_possible_spots.contains(&s) {
+        unique_possible_spots.push(s);
+      }
+    }
+
+    println!("{}", unique_possible_spots.len());
 }
 
 fn gets_stuck(obstacles: &Vec<(usize, usize)>, mut guard_pos: (usize, usize), mut facing: Dir, max_x: usize, max_y: usize, extra_obstacle: (usize, usize)) -> bool {
+  if obstacles.contains(&extra_obstacle) {
+    return false;
+  }
+  
   let mut path = Vec::new();
   while guard_pos.0 < max_x
-    && guard_pos.1 < max_y {
-    if path.contains(&(guard_pos.0, guard_pos.1, facing.clone())) {
-      return true;
+      && guard_pos.1 < max_y {
+      if path.contains(&(guard_pos.0, guard_pos.1, facing.clone())) {
+        return true;
+      }
+      path.push((guard_pos.0, guard_pos.1, facing.clone()));
+      if let Some(next_pos) = facing.step(guard_pos) {
+        if obstacles.contains(&next_pos) || extra_obstacle == next_pos {
+          facing = facing.next();
+        } else {
+          guard_pos = next_pos;
+        }
+      } else {
+        break; // reached map end top/left
+      }
     }
-
-    path.push((guard_pos.0, guard_pos.1, facing.clone()));
-    match facing {
-      Dir::up => {
-        if obstacles.contains(&(guard_pos.0, guard_pos.1 - 1)) || extra_obstacle == (guard_pos.0, guard_pos.1 - 1) {
-          facing = Dir::right;
-        } else if guard_pos.1 == 0 {
-          return false;
-        } else {
-          guard_pos = (guard_pos.0, guard_pos.1 - 1)
-        }
-      },
-      Dir::left => {
-        if obstacles.contains(&(guard_pos.0 -1 , guard_pos.1)) || extra_obstacle == (guard_pos.0 -1 , guard_pos.1) {
-          facing = Dir::up;
-        } else if guard_pos.0 == 0 {
-          return false;
-        }  else {
-          guard_pos = (guard_pos.0 - 1, guard_pos.1)
-        }
-      },
-      Dir::right => {
-        if obstacles.contains(&(guard_pos.0 + 1, guard_pos.1)) || extra_obstacle == (guard_pos.0 + 1, guard_pos.1) {
-          facing = Dir::down;
-        } else {
-          guard_pos = (guard_pos.0 + 1, guard_pos.1)
-        }
-      },
-      Dir::down => {
-        if obstacles.contains(&(guard_pos.0, guard_pos.1 + 1)) || extra_obstacle == (guard_pos.0, guard_pos.1 + 1) {
-          facing = Dir::left;
-        } else {
-          guard_pos = (guard_pos.0, guard_pos.1 + 1)
-        }
-      },
-    }
-  }
-
+  
   false
 }
 
 #[derive(PartialEq, Debug, Clone)]
 enum Dir {
-    up,
-    left,
-    right,
-    down,
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+impl Dir {
+    pub fn next(&self) -> Dir {
+      match self {
+        Dir::Up => Dir::Right,
+        Dir::Left => Dir::Up,
+        Dir::Right => Dir::Down,
+        Dir::Down => Dir::Left,
+      }
+    }
+
+    pub fn step(&self, old: (usize, usize)) -> Option<(usize, usize)> {
+      if (matches!(self, Dir::Left) && old.0 == 0)
+      || (matches!(self, Dir::Up) && old.1 == 0) {
+        return None;
+      }
+
+      match self {
+        Dir::Up => Some((old.0, old.1 - 1)),
+        Dir::Left => Some((old.0 -1 , old.1)),
+        Dir::Right => Some((old.0 + 1, old.1)),
+        Dir::Down => Some((old.0, old.1 + 1)),
+      }
+    }
 }
