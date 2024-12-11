798de87b1026d5e476f47ea71a6b07a85aea95b5
NobodyForNothing <contact@derdilla.com>
2024-12-11 09:33:31 +0100

    Solve 11

--- src/11.dart
+++ src/11.dart
@@ -1,4 +1,5 @@
 import '../aoc_solver.dart';
+import 'dart:math';
 
 void main(List<String> _args) {
   final solver = A11()
@@ -15,32 +16,39 @@ class A11 extends AOCSolver<String> {
   String parse(String input) => input;
 
   @override
-  String computeA(String input) {
-    List<String> sto = input.split(' ');
-    List<String> nextSo = [];
-    for (int i = 0; i < 25; i++) {
-      log(i);
-      for (final s in sto) {
-        if (int.parse(s) == 0) {
-          nextSo.add('1');
-        } else if (s.length % 2 == 0) {
-          log(s);
-          log(s.substring(0, s.length ~/ 2));
-          log(s.substring( s.length ~/ 2, s.length));
-          nextSo.add(int.parse(s.substring(0, s.length ~/ 2)).toString());
-          nextSo.add(int.parse(s.substring( s.length ~/ 2, s.length)).toString());
-        } else {
-          nextSo.add((int.parse(s) * 2024).toString());
-        }
-      }
-      sto = nextSo;
-      nextSo = [];
-    }
-    return sto.length.toString();
-  }
+  String computeA(String input) => input
+    .split(' ')
+    .map(int.parse)
+    .map((e) => recCount(e, 25))
+    .fold(0, (acc, x) => acc + x)
+    .toString();
 
   @override
-  String computeB(String input) {
-    throw UnimplementedError();
+  String computeB(String input) => input
+    .split(' ')
+    .map(int.parse)
+    .map((e) => recCount(e, 75))
+    .fold(0, (acc, x) => acc + x)
+    .toString();
+
+
+  final cache = <(int, int), int>{};
+  int recCount(int i, int n) {
+    if (cache[(i,n)] != null) return cache[(i,n)]!;
+    if (n == 0) return 1;
+
+    int res = 0;
+    if (i == 0) {
+      res = recCount(1, n-1);
+    } else if (((log(i) / log(10)).floor() + 1) % 2 == 1) {
+      res = recCount(i * 2024, n - 1);
+    } else {
+      final digitCount = ((log(i) / log(10)).floor() + 1);
+      res = (recCount(i ~/ pow(10, digitCount ~/ 2).toInt(), n - 1) + 
+          recCount(i % pow(10, digitCount ~/ 2).toInt(), n - 1));
+    }
+
+    cache[(i,n)] = res;
+    return res;
   }
 }
